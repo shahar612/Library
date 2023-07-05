b675a0fdb7b11147650a2af56c9da7fd70ea8d77
Ariel Slavinsky <nadav8.tch15@gmail.com>
2023-07-05 16:06:50 +0300

    Added Book class

---
+++ Library/src/Book.java
@@ -0,0 +1,26 @@
+
+public abstract class Book implements Comparable {
+    protected String bookName;
+    protected int pageNumber;
+
+    public Book(String bookName, int pageNumber) {
+        this.bookName = bookName;
+        this.pageNumber = pageNumber;
+    }
+
+    public String getBookName() {
+        return this.bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public int getPageNumber() {
+        return this.pageNumber;
+    }
+
+    public void setPageNumber(int pageNumber) {
+        this.pageNumber = pageNumber;
+    }
+}
